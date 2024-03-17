53ae01f0d5b5859c732ddd638bc7dcd85786d787
zackSoftware <143184330+zackSoftware@users.noreply.github.com>
2024-03-17 18:02:18 +0100

    Script init

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+shared_scripts {
+    'shared/*.lua'
+}
+
+server_scripts {
+    'server/*.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+
+dependencies {
+    'qb-core',
+    'ox_lib'
+}
---
+++ shared/config.lua
@@ -0,0 +1,6 @@
+Config = {}
+
+Config.Locations = {
+    Cooking = vector3(0, 0, 0)
+
+}
