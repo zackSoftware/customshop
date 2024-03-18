c6c0c75f17c579f4a8efeaa050538e153264031e
zackSoftware <143184330+zackSoftware@users.noreply.github.com>
2024-03-18 17:00:47 +0100

    Adding recipes and items images

--- client/cl.lua
+++ client/cl.lua
@@ -1,1 +1,24 @@
 QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent('QBCore:Client:OnJobUpdate')
+AddEventHandler('QBCore:Client:OnJobUpdate', function(JobInfo)
+  PlayerJob = JobInfo
+  onDuty = PlayerJob.onduty
+end)
+
+RegisterNetEvent('QBCore:Client:SetDuty')
+AddEventHandler('QBCore:Client:SetDuty', function(duty)
+  onDuty = duty
+end)
+
+Citizen.CreateThread(function()
+    CustomShop = AddBlipForCoord(-1197.32, -897.655, 13.995)
+    SetBlipSprite (CustomShop, 106)
+    SetBlipDisplay(CustomShop, 4)
+    SetBlipScale  (CustomShop, 0.5)
+    SetBlipAsShortRange(CustomShop, true)
+    SetBlipColour(CustomShop, 75)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentSubstringPlayerName("CustomShop")
+    EndTextCommandSetBlipName(CustomShop)
+end) 
--- shared/config.lua
+++ shared/config.lua
@@ -19,3 +19,77 @@ Config.Locations = {
         bossStash = vector3(0, 0, 0)
     }
 }
+
+Config.Recipes = {
+    big_piggy_burger = {
+        RequiredItems = {
+            ["burgerbuns"] = 1,
+            ["burgermeatpatty_cooked"] = 1,
+            ["bacon"] = 3,
+            ["cheese"] = 1,
+        }
+    },
+    heart_attack_burger= {
+        RequiredItems = {
+            ["burgerbuns"] = 1,
+            ["burgermeatpatty_cooked"] = 3,
+            ["cheese"] = 1,
+        }
+    },
+    filthy_crim_burger= {
+        RequiredItems = {
+            ["burgerbuns"] = 1,
+            ["burgermeatpatty_cooked"] = 1,
+            ["cheese"] = 1,
+        }
+    },
+    danny_burger= {
+        RequiredItems = {
+            ["burgerbuns"] = 1,
+            ["burgermeatpatty_cooked"] = 1,
+            ["cheese"] = 1,
+        }
+    },
+    vegetarian_burger= {
+        RequiredItems = {
+            ["burgerbuns"] = 1,
+            ["burgervegetablepatty_raw"] = 1,
+            ["mixedvegetables"] = 1,
+        }
+    },
+    loaded_fries= {
+        RequiredItems = {
+            ["choppedpotato"] = 1,
+        }
+    },
+    french_fries= {
+        RequiredItems = {
+            ["choppedpotato"] = 1,
+        }
+    },
+    sweet_potato_fries= {
+        RequiredItems = {
+            ["sweetpotato"] = 1,
+        }
+    },
+    loaded_thickshake= {
+        RequiredItems = {
+            ["chocolate"] = 1,
+            ["milk"] = 1,
+            ["icecream"] = 1,
+            ["chocolatesyrup"] = 1,
+        }
+    },
+    chocolate_milkshake= {
+        RequiredItems = {
+            ["chocolate"] = 1,
+            ["milk"] = 1,
+        }
+    },
+    strawberry_milkshake= {
+        RequiredItems = {
+            ["strawberries"] = 1,
+            ["milk"] = 1,
+        }
+    }
+}
